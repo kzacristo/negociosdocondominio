bbc196281328fa527ccd5d4dd1bc2f9216792c0e
bruno.moura <bruno.moura@ws-solution.com>
2022-07-01 12:21:15 -0300

    realizado a melhoria do buscar perfil

---
+++ negociosdocondominio/buscarperfil.php
@@ -0,0 +1,133 @@
+<?php
+$email = $_POST['email'] ;
+$id =  $_POST['id'];
+//var_dump($_POST['email'], $_POST['id']); die();
+require_once('./controller/conect.php');
+
+try {
+  $stmt = $pdo->prepare("SELECT m.* , s.*  FROM morador m LEFT JOIN servicos s ON s.id_morador = m.id WHERE m.id = $id ORDER BY m.id DESC ");
+  $stmt->execute();
+
+  $row2 = $stmt->fetchAll();
+//   var_dump($row); die();
+
+
+  if (!isset($row2) && sizeof($row2) <= 0) {
+    header('location:index.php');
+  }
+} catch (PDOException $e) {
+  echo 'ERROR: ' . $e->getMessage();
+}
+$idmorador = (isset($row2) && (int)$row2[0]['id'] > 0 ) ? $row2[0]['id'] : 0;
+
+$dataatendimento = (isset($row2[0]['data_atendimento'])) ? explode(',',$row2[0]['data_atendimento']) : false;
+
+$trabalho = (isset($row2[0]['data_atendimento']) && sizeof($dataatendimento) > 0) ? $row2[0]['data_atendimento'] : false;
+if ($trabalho) $trabalho = explode(',', $trabalho);
+
+// var_dump($redesocial); die();
+// var_dump($row); die();
+if ($row[0]['redes_sociai'] = 'sim'){
+  $query = $pdo->prepare("SELECT r.* FROM redes_sociais r WHERE r.id_morador = $idmorador");
+  $query->execute();
+
+  $redesocial= $query->fetchAll();
+} 
+
+include "header.php";
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Perfil</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="destaqueperfil"> 
+        <?php if (isset($row2[0]['imagem']) && $row2[0]['imagem'] != '' ) : ?>
+            <img src="<?php echo $row2[0]['imagem'] ?>" height="100">
+          <?php else : ?>
+            <img src="./images/images.jpeg" height="100">
+          <?php endif ?>
+          <h2><?php echo $row2[0]['nome'] ?></h2>
+          <p><?php echo $row2[0]['titulo_anuncio'] ?></p>
+          <br>
+          <?php if ($row2[0]['valor']) : ?>
+            <h2>Valor</h2>
+            <?php
+            switch ($row2[0]['tipo_valor']) {
+              case 1:
+                $tipo_valor = 'Por Hora';
+                break;
+              case 2:
+                $tipo_valor = 'Por Dia';
+                break;
+              case 3:
+                $tipo_valor = 'Por Serviço';
+                break;
+              default:
+                $tipo_valor = 'A combinar';
+                break;
+            }
+
+            ?>
+            <p><?php $valor =  $tipo_valor . ' :' . ' R$. ' . $row[0]['valor'];
+                echo $valor ?></p>
+            <br>
+          <?php endif ?>
+          <h2>Informações de contato</h2>
+          <p>Telefone: <?php echo $row2[0]['telefone'] ?></p>
+          <p>Whatsapp: <?php echo $row2[0]['whatsapp'] ?></p>
+          <p>E-mail: <?php echo $row2[0]['email'] ?></p>
+          <br>
+          <?php if (isset($trabalho) && $trabalho != '') : ?>
+            <h2>Horário de funcionamento</h2>
+            <?php foreach ($trabalho as $key => $semanal) : ?>
+              <p><?php echo $semanal ?></p>
+            <?php endforeach ?>
+          <?php endif ?>
+          <br>
+          <?php if ($redesocial) : ?>
+            <h2>Redes socias</h2>
+            <?php foreach ($redesocial as $key => $link) : ?>
+                <p>LinkedIn: <a href="<?php echo $link['linkedin'] ?>">LinkedIn</a> </p>
+                <p>Facebook:<a href="<?php echo $link['facebook'] ?>">Facebook</a></p>
+                <p>Twitter:<a href="<?php echo $link['twitter'] ?>">Twitter</a></p>
+                <p>Google+:<a href="<?php echo $link['googleplus'] ?>">Google+</a></p>
+                <p>YouTube:<a href="<?php echo $link['youtube'] ?>">YouTube</a></p>
+                <p>Instagram:<a href="<?php echo $link['instagram'] ?>">Instagram</a></p>
+            <?php endforeach ?>
+          <?php endif ?>
+          </div>
+      </div>
+      <div class="col-sm-8">
+        <div class="frasedestaque">
+          <h1 class="display-6 branco"><?php echo $row2[0]['titulo_anuncio'] ?></h1>
+        </div>
+        <?php if ($row2[0]['sobre_oque_faz']) : ?>
+          <h1 class="titulo">Sobre seu trabalho e experiência</h1>
+          <?php echo $row2[0]['sobre_oque_faz'] ?>
+        <?php endif ?>
+        <?php if ($row[0]['sobre_voce']) : ?>
+          <h1 class="titulo">Mais sobre informaçoes sobre Lucia Costa </h1>
+          <?php echo $row2[0]['sobre_voce'] ?>
+        <?php endif ?>
+          </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
+
+<?php
+
+include "footer.php"
+
+?>
--- negociosdocondominio/controller/pesquisar.php
+++ negociosdocondominio/controller/pesquisar.php
@@ -39,7 +39,7 @@ foreach ($where as $k => $v) {
 }
 
 try {
-    $stmt = $pdo->prepare("SELECT s.*, m.nome, m.email, m.telefone, m.whatsapp FROM servicos s LEFT JOIN morador m ON m.id = s.id_morador WHERE $w ");
+    $stmt = $pdo->prepare("SELECT s.*, m.nome, m.email, m.telefone, m.whatsapp, m.imagem FROM servicos s LEFT JOIN morador m ON m.id = s.id_morador WHERE $w ");
     $stmt->execute();
 
     $row = $stmt->fetchAll();
@@ -66,14 +66,15 @@ try {
         foreach($row as $key => $value){
             $email = $value['email'];
             $idmorador = $value['id_morador'];
-            $imagem = $value['imagem'];
-            $result .= "<td>"."<a class='nav-link active' aria-current='page' href='login.php'><img src='<?php echo $imagem?>' class='logo'>"."</td>";
+            $imagem = (isset($value['imagem'])) ? $value['imagem'] : '../images/images.jpeg';
+            //var_dump($value['id_morador']); 
+            $result .= "<td>"."<img src='$imagem' width='32' height='32' class='rounded-circle'>"."</td>";
             $result .= "<td>".$value['nome']."</td>";
             $result .= "<td>".$value['email']."</td>";
             $result .= "<td>".$value['telefone']."/".$value['whatsapp']."</td>";
             $result .= "<td>".$value['area_de_atuacao']."</td>";
             $result .= "<td>".$value['titulo_anuncio']."</td>";
-            $result .= "<td>"."<form action='../controller/cadastrosemservico.php' method='post' enctype='multipart/form-data'> <input type='hidden' class='form-control' id='email' name='email' value='<?php echo $email ?>' <td>"." <input type='hidden' class='form-control' id='id' name='id' value='<?php echo $id ?>' />"."<button  type='submit' class='btn btn-danger' data-bs-toggle='modal' data-bs-target='#visitarperfil' >Visitar Perfil</button></form>"."</td>";
+            $result .= "<td>"."<form action='../buscarperfil.php' method='post' enctype='multipart/form-data'> <input type='hidden' class='form-control' id='email' name='email' value='$email' <td>"." <input type='hidden' class='form-control' id='id' name='id' value='$idmorador' />"."<button  type='submit' class='btn btn-danger' data-bs-toggle='modal' data-bs-target='#visitarperfil' >Visitar Perfil</button></form>"."</td>";
             $result .="</tr>";
         }
         echo $result .="</tbody></table>";
